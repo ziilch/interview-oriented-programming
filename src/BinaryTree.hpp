c97a29717484f68bded7e441eb0010788ab2addd
zorro <zorro@zorrodeMacBook-Pro.local>
2020-04-24 01:00:00 +0800

    新增六道树的题目解法

--- src/BinaryTree.hpp
+++ src/BinaryTree.hpp
@@ -7,16 +7,93 @@
 
 #include "../includes/BaseInclude.h"
 
+// 九阳真经-树
+// 树是一个天然递归的结构
+// 做树问题的时候往往要想树的每一层需要做什么
+// 然后想想怎么把每一层做的东西返回
+// 最后形成一个新的结构
+// 二叉树的题目最开始接触的时候会不知所云，挠头苦想
+// 这个时候不要纠结，看看答案，在没有完全理解的时候也去做后面的题目
+// 做多了，回头看，感觉就有了
+// 再巩固一下，一般的题目就可以做出来了。
+
 class BinaryTree{
 public:
     // M-L105-****
     // 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return dfsJ7(preorder, inorder);
+        return dfsL105(preorder, inorder);
+    }
+
+    // E-L226-****
+    // 返回一个二叉树的镜像
+    TreeNode* mirrorTree(TreeNode* root) {
+        dfsL226(root);
+        return root;
+    }
+
+    // E-L101-****
+    // 判断一个二叉树是不是对称的
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL) return true;
+        return dfsL101(root->left, root->right);
+    }
+
+    // E-J54-***
+    // 输出一颗二叉搜索树的第k大节点
+    int kthLargest(TreeNode* root, int k) {
+        int cnt = 1, res = 0;
+        dfsJ54(root, cnt, k, res);
+        return res;
+    }
+
+    // E-L104-****
+    // 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+
+    // E-L110-***
+    // 判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL) return true;
+        return abs(maxDepth(root->left) - maxDepth(root->right)) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+    }
+
+    // E-L235-**
+    // 二叉搜索树的最近公共祖先
+    TreeNode* lowestCommonAncestor1(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL) return root;
+        if (p->val < root->val && q->val < root->val) return lowestCommonAncestor1(root->left, p, q);
+        else if (p->val > root->val && q->val > root->val) return lowestCommonAncestor1(root->right, p, q);
+        else return root;
+    }
+
+    // M-L236-****
+    // 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left == NULL) return right;
+        if (right == NULL) return left;
+        return root;
     }
 
 private:
-    TreeNode* dfsJ7(vector<int>& preorder, vector<int>& inorder) {
+    void dfsJ54(TreeNode* root, int& cnt, int k, int& res) {
+        if (root == NULL) return;
+        dfsJ54(root->right, cnt, k, res);
+        if (cnt++ == k) {
+            res = root->val;
+            return;
+        }
+        dfsJ54(root->left, cnt, k, res);
+    }
+
+    TreeNode* dfsL105(vector<int>& preorder, vector<int>& inorder) {
         if (preorder.size() == 0) return NULL;
         TreeNode* root = new TreeNode(preorder[0]);
         int pos = 0;
@@ -28,9 +105,28 @@ private:
         for (int i = pos + 1; i < preorder.size(); i++) mPre.push_back(preorder[i]);
         for (int i = pos + 1; i < preorder.size(); i++) mIn.push_back(inorder[i]);
 
-        root->left = dfsJ7(nPre, nIn);
-        root->right = dfsJ7(mPre, mIn);
+        root->left = dfsL105(nPre, nIn);
+        root->right = dfsL105(mPre, mIn);
         return root;
+    }
+
+    void dfsL226(TreeNode* root) {
+        if (root == NULL) return;
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        dfsL226(root->left);
+        dfsL226(root->right);
+    }
+
+    bool dfsL101(TreeNode* t1, TreeNode* t2) {
+        if (t1 == NULL || t2 == NULL)
+            return (t1 == NULL && t2 == NULL);
+        else
+            if (t1->val == t2->val)
+                return dfsL101(t1->left, t2->right) && dfsL101(t1->right, t2->left);
+            else
+                return false;
     }
 };
 
